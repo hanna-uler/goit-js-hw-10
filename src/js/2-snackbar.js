34bf4e881b9b16ade8b39530943343862bfb883c
hanna-uler <hannauler@gmail.com>
2025-03-11 02:08:53 -0400

    start snackbar styles, import iziToast to snackbar.js

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,23 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+// 
+// Playing with iziToast
+// const formEl = document.querySelector(".form");
+// formEl.addEventListener("click", alert);
+// function alert(event) {
+//     if (formEl.querySelector(".rtf").textContent === "") {
+//            return iziToast.success({
+//                 title: 'OK',
+//                 message: 'Please choose a date in the future',
+//                 closeOnClick: true,
+//                 position: 'topRight',
+//                 timeout: 3000,
+//                 backgroundColor: #326101,
+// messageColor: #FFFFFF,
+// messageSize: 16px, 
+// messageLineHeight: 24px, 
+                    
+//            });
+//     }
+// };
